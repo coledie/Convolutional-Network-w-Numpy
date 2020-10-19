904ad742890b7a91612dbb350e07936759534333
csdhv9 <csdhv9@umsystem.edu>
2020-10-19 17:14:58 -0500

    Update module usage.

--- activation.py
+++ activation.py
@@ -6,8 +6,8 @@ import numpy as np
 from module import Module
 
 
-class ReLu:
-    def __init__(self, leak):
+class ReLu(Module):
+    def __init__(self, leak=0):
         super().__init__()
 
         self.leak = leak
@@ -18,10 +18,10 @@ class ReLu:
         return self.output
 
     def backward(self, error):
-        return (np.where(x > 0, 1, self.leak)) * error
+        return (np.where(error > 0, 1, self.leak)) * error
 
 
-class Sigmoid:
+class Sigmoid(Module):
     def forward(self, x):
         self.output = np.tanh(x)
         return self.output
--- layer.py
+++ layer.py
@@ -6,7 +6,7 @@ import numpy as np
 from module import Module
 
 
-class Linear:
+class Linear(Module):
     def __init__(self, n_inputs, n_outputs):
         super().__init__()
 
@@ -24,4 +24,40 @@ class Linear:
         return np.matmul(error, self.weight)
 
 
-__ALL__ = [Linear]
+class MaxPool(Module):
+    """
+    Max pool layer for 2d inputs.
+
+    Parameters
+    ----------
+    size: int
+        Size of kernel.
+    stride: int, default=size
+        How far kernel moves each step.
+    padding: int
+    """
+    def __init__(self, size, stride=None):
+        super().__init__()
+        
+        self.size = size
+        self.stride = stride or size
+
+    def forward(self, x):
+        # Split up into blocks based on kernel size and stride
+        s = self.stride
+        x_split = []
+        for i in range(x.shape[0] // self.stride):
+            x_split.append([])
+            for j in range(x.shape[1] // self.stride):
+                x_split[-1].append(x[i*s:(i+1)*s, j*s:(j+1)*s])
+
+        output = np.max(x_split, axis=-1)
+        return output
+
+    def backward(self, error):
+        # Assumes x is one dimensional
+        error = error.reshape((-1, 1))
+        return np.stack([error] * self.size, axis=-1).flatten()
+
+
+__ALL__ = [Linear, MaxPool]
